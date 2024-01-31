c314ef66a5edaac565cd192c1780296c710f6dda
Zeeta:) <ebdtv4@gmail.com>
2024-01-31 18:32:49 +0700

    <ui>(Push Hero Content Section)

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -14,6 +14,10 @@ function Hero() {
           <h3 className="text-6xl mt-10 text-[#061326] font-bold xl:w-1/4 leading-[70px]">Peaceful shopping. mindful money</h3>
           <p className="text-sm xl:w-1/5 mt-6 leading-6 text-gray-400 font-medium">See how we can help with making your shopping experience and money management more TWIN</p>
         </div>
+        <div className="for-banner-support">
+          <div></div>
+          <div></div>
+        </div>
       </div>
       <div className="right"></div>
     </div>
